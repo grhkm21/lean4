d42ab14df3e479901ec4e61aaacc195e34b2df46
Gareth Ma <grhkm21@gmail.com>
2023-10-15 13:50:36 +0100

    IUM2023: Finished problem 4 & 5

--- Lean4/IUM2023/Sheet1.lean
+++ Lean4/IUM2023/Sheet1.lean
@@ -105,6 +105,36 @@ theorem Problem (hx : x ≠ 0) (h : a * x = b * x) : a = b := by
     · rw [succ_mul, succ_mul, Nat.add_right_cancel_iff] at h
       rw [ha h]
 
+/- Alternative solution below without induction -/
+lemma le_or_le (x y : ℕ) : x ≤ y ∨ y ≤ x := by
+  induction' x with x hx
+  · left
+    exact Nat.zero_le y
+  · cases' hx with h h
+    · let ⟨z, hz⟩ := Nat.le.dest h
+      cases' z with z ; FIX
+      · right
+        rw [←hz, add_zero]
+        exact le_succ x
+      · left
+        rw [←hz, add_succ, succ_eq_add_one, succ_eq_add_one, add_comm x z, add_assoc, add_comm z]
+        exact le_add_right _ _
+    · right
+      exact le_trans h (le_succ x)
+
+example (h : a ≤ b) : ∃ c, a + c = b := le.dest h
+lemma eq_of_mul_eq_right_and_le (h : x ≠ 0) (h' : a * x = b * x) (hab : a ≤ b) : a = b := by
+  let ⟨c, hc⟩ := Nat.le.dest hab
+  rw [←hc, ←add_zero (a * x), add_mul] at h'
+  have hcx : c = 0 ∨ x = 0 := Nat.mul_eq_zero.mp (add_left_cancel h'.symm)
+  have hc' : c = 0 := (or_iff_not_imp_right.mp hcx) h
+  rw [←hc, hc', add_zero]
+
+lemma mul_left_cancel (h : x ≠ 0) (h' : a * x = b * x) : a = b := by
+  cases' le_or_le a b with hab hab
+  · exact eq_of_mul_eq_right_and_le h h' hab
+  · exact (eq_of_mul_eq_right_and_le h h'.symm hab).symm
+
 end Problem4
 
 -------------------------------------------------------------------------------
@@ -155,21 +185,42 @@ theorem PartC' (h : a * b = c) (ha : 1 < a) (hb : 1 < b) : a < c := by
 theorem PartC (h : a * b = c) (ha : 1 < a) (hb : 1 < b) : a < c ∧ b < c := by
   exact ⟨PartC' h ha hb, PartC' (by rw [←h, mul_comm]) hb ha⟩
 
--- What is the definition of .Prime? The `irreducible` one?
+/- What is the definition of .Prime? The `irreducible` one? -/
 theorem PartD (h : 1 < c) (hc : ¬c.Prime) : ∃ a b, c = a * b ∧ 1 < a ∧ a < c ∧ 1 < b ∧ b < c := by
   rw [Nat.prime_iff, _root_.Prime] at hc
   push_neg at hc
   rw [Nat.isUnit_iff] at hc
   let ⟨a, b, h₁, h₂, h₃⟩ := hc (not_eq_zero_of_lt h) (Nat.ne_of_gt h)
+  sorry
 
+/- This should be earlier -/
+theorem PartE (h : a * b = 0) : a = 0 ∨ b = 0 := by
+  by_contra' h'
+  let ⟨a, ha⟩ := exists_eq_succ_of_ne_zero $ h'.left
+  let ⟨b, hb⟩ := exists_eq_succ_of_ne_zero $ h'.right
+  rw [ha, hb, succ_mul, add_succ] at h
+  exact succ_ne_zero _ h
 
-example (h : ¬c.Prime) : ∀ a (ha : 1 < a ∧ a < c), ¬a ∣ c := by
-  exact?
+theorem PartF (h : a * b = a) : a = 0 ∨ b = 1 := by
+  match b with
+  | zero => FIX ; left ; rw [←h, mul_zero]
+  | succ b =>
+      nth_rw 2 [←zero_add a] at h
+      rw [one_eq_succ_zero, succ_inj']
+      exact PartE (add_right_cancel h)
 
-theorem PartE (h : a * b = 0) : a = 0 ∨ b = 0 := by sorry
+lemma PartG' (h : a ≤ 1) (h' : a ≠ 0) : a = 1 := Problem2.PartB h (zero_lt_of_ne_zero h')
 
-theorem PartF (h : a * b = a) : a = 0 ∨ b = 1 := by sorry
-
-theorem PartG (h : a * b = 1) : a = 1 ∧ b = 1 := by sorry
+theorem PartG (h : a * b = 1) : a = 1 ∧ b = 1 := by
+  have h' : a ≤ 1 ∧ b ≤ 1 := PartB h one_ne_zero
+  have h'' : a ≠ 0 ∧ b ≠ 0 := by
+    by_contra h
+    rw [not_and_or, not_not, not_not] at h
+    cases' h with ha hb
+    · rw [ha, zero_mul] at h
+      exact zero_ne_one h
+    · rw [hb, mul_zero] at h
+      exact zero_ne_one h
+  exact ⟨PartG' h'.left h''.left, PartG' h'.right h''.right⟩
 
 end Problem5
--- Lean4/cp.lean
+++ Lean4/cp.lean
@@ -1,10 +1,13 @@
 import Mathlib.Tactic
 
-def f (α : Type) := ∀ a : α, a = a
+open Nat
 
-noncomputable instance [DecidableEq α] : Decidable (f α) := Classical.dec (f α)
+example {m n : ℕ} (h : n ≠ 0) : ∃ m : ℕ, n = m.succ := exists_eq_succ_of_ne_zero h
 
-instance [DecidableEq α] [Fintype α] : Decidable (f α) := inferInstanceAs (Decidable (∀ _, _))
-
-#eval f ℚ
-#eval f ({1, 2, 3} : Finset ℤ)
+example {a b : ℕ} : a < b ∨ a = b ∨ a > b := Nat.lt_trichotomy a b
+example {a b : ℕ} : a ≤ b ∨ b ≤ a := Nat.le_or_le a b
+example : False → P := by exact?
+example {a : ℕ} : a ≤ succ a := by exact?
+example (h : succ a ≤ succ b) : a ≤ b := by exact?
+example (h : ¬P ∨ Q) (h' : P) : Q := by exact?
+example (h : ¬P ∨ Q) (h' : P) : Q := imp_iff_not_or.mpr h h'
